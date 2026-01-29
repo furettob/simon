b7ef961c4de4839881b92d6d056546b62255e145
spensieri-s <stefano.spensieri@docity.com>
2026-01-29 19:07:15 +0100

    Font faces p1

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import './index.css'
-import SimonGame from '@/components/SimonGame/SimonGame'
+import SimonApp from '@/components/SimonApp/SimonApp'
 import { simonReducer, initialState } from '@/utils/simonReducer'
 
 const simonStore = configureStore({
@@ -16,7 +16,7 @@ const simonStore = configureStore({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={simonStore}>
-      <SimonGame />
+      <SimonApp />
     </Provider>
   </StrictMode>,
 )
