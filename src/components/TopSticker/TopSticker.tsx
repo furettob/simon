8e66a9295d2a0ee639461fe5c1145b157ee64081
spensieri-s <stefano.spensieri@docity.com>
2026-02-04 00:18:26 +0100

    Adjust CSS for real mobile

--- src/components/TopSticker/TopSticker.tsx
+++ src/components/TopSticker/TopSticker.tsx
@@ -3,16 +3,16 @@ import classNames from "classnames";
 
 const TopSticker = () => {
   return (
-      <div className={styles.topStickerWrapper}>
-        <div className={styles.topStickerSticker}>
-          <div className={styles.topStickerContent}>
-            <div className={styles.topStickerLogoWrapper}>
-              <div className={styles.topStickerLogoWithDecoration}>
-                <span className={classNames(styles.topStickerLogo, styles.topStickerLogoF)}>F</span>
-                <span className={classNames(styles.topStickerLogo, styles.topStickerLogoB)}>B</span>
-                <span className={styles.topStickerTextDecoration}>Ⓡ</span>
+      <div className={styles.wrapper}>
+        <div className={styles.silverBg}>
+          <div className={styles.content}>
+            <div className={styles.logoWrapper}>
+              <div className={styles.logoContent}>
+                <span className={classNames(styles.logo, styles.logoF)}>F</span>
+                <span className={classNames(styles.logo, styles.logoB)}>B</span>
+                <span className={styles.logoRegisteredSymbol}>Ⓡ</span>
               </div>
-              <div className={styles.topStickerText}>FurettoB</div>
+              <div className={styles.logoText}>FurettoB</div>
             </div>
           </div>
         </div>
