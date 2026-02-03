b0c2358b93f93b71df82801f7be03a99e34d1244
spensieri-s <stefano.spensieri@docity.com>
2026-02-03 16:01:48 +0100

    Clean unused features

--- src/components/SimonApp/SimonApp.tsx
+++ src/components/SimonApp/SimonApp.tsx
@@ -1,51 +1,11 @@
-import { useSelector } from "react-redux";
-import type { SimonState } from "@/utils/simonReducer";
-import { ColorIndicator } from "@/components/ColorIndicator/ColorIndicator";
 import { Stack } from "@mui/system";
 import styles from "./SimonApp.module.scss";
 import SimonDevice from "@/components/SimonDevice/SimonDevice";
 
-const SimonGame = () => {
-  const { sequenceLength, completeSequence, playerSequence } = useSelector(
-    (state: SimonState) => state,
-  );
-
-  return (
-    <div>
-      <Stack alignItems="center" className={styles.simonAppContainer}>
+const SimonGame = () => (
+      <Stack alignItems="center" justifyContent="center" className={styles.simonAppContainer}>
         <SimonDevice />
       </Stack>
-      <div className={styles.tmp}>
-        <Stack
-          spacing={6}
-          padding={6}
-          direction="row"
-          alignItems="center"
-          justifyContent="stretch"
-        >
-          <div>
-            <h1>Simon Game</h1>
-            <div>
-              <p>Level: {sequenceLength}</p>
-            </div>
-          </div>
-        </Stack>
-        <div className={styles.debugContainer}>
-          <h3>Debug Info:</h3>
-          <Stack direction="row" spacing={1}>
-            <span className={styles.debugLabel}>GS: </span>
-            {completeSequence.map((colorIndex: number, index: number) => (
-              <ColorIndicator
-                key={`${index}_${colorIndex}`}
-                colorIndex={colorIndex}
-                isHighlighted={index === playerSequence.length}
-              />
-            ))}
-          </Stack>
-        </div>
-      </div>
-    </div>
   );
-};
 
 export default SimonGame;
