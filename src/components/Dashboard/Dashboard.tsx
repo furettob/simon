f4b58082c70255f0a03dffddbcf70ea250b5f86a
spensieri-s <stefano.spensieri@docity.com>
2026-02-11 00:36:30 +0100

    Info assets and copy

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,21 +5,31 @@ import classNames from "classnames";
 import ShareIcon from "@mui/icons-material/Share";
 import InfoIcon from "@mui/icons-material/Info";
 import { Stack } from "@mui/system";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import ReactGA from "react-ga4";
 import { getLongestSequenceInMemory } from "@/utils/sequence";
+import InfoDialog from "@/components/InfoDialog/InfoDialog";
 
 const Dashboard = () => {
   // Use selectors to get state
-  const { sequenceLength, longestSequence: longestSequenceInState} = useSelector((state: SimonState) => state);
+  const [isInfoDialogOpen, setIsInfoDialogOpen] = useState(false);
+  const { sequenceLength, longestSequence: longestSequenceInState } =
+    useSelector((state: SimonState) => state);
 
   const level = sequenceLength > 0 ? `0${sequenceLength}`.slice(-2) : "--";
 
-  const shareText = useCallback(async (): Promise<"shared" | "copied"> => {
-    const longestSequence = getLongestSequenceInMemory({longestSequenceInState})
-    const introText = longestSequence.length >= 5 ? `I reached level ${longestSequence.length} at 'Simon Says' game!`: `I just played 'Simon Says' game!`
-    const title =  `80s nostalgia!`
-    const text = `${introText} Try it at https://furettob.github.io/simon ${COLORS_EMOJI[0]}${COLORS_EMOJI[1]}${COLORS_EMOJI[2]}${COLORS_EMOJI[3]}`
+  const handleShareClicked = useCallback(async (): Promise<
+    "shared" | "copied"
+  > => {
+    const longestSequence = getLongestSequenceInMemory({
+      longestSequenceInState,
+    });
+    const introText =
+      longestSequence.length >= 5
+        ? `I reached level ${longestSequence.length} at 'Simon Says' game!`
+        : `I just played 'Simon Says' game!`;
+    const title = `80s nostalgia!`;
+    const text = `${introText} Try it at https://furettob.github.io/simon ${COLORS_EMOJI[0]}${COLORS_EMOJI[1]}${COLORS_EMOJI[2]}${COLORS_EMOJI[3]}`;
 
     // Try native share first
     if (navigator.share) {
@@ -32,10 +42,10 @@ const Dashboard = () => {
         ReactGA.event({
           category: "share",
           action: "share button clicked",
-          label: "sharing", 
-          value: 2
+          label: "sharing",
+          value: 2,
         });
-        return 'shared'
+        return "shared";
       } catch {
         // user cancelled or share failed → fallback
       }
@@ -45,8 +55,18 @@ const Dashboard = () => {
     return "copied";
   }, [longestSequenceInState]);
 
+  const handleInfoClicked = useCallback(() => {
+    setIsInfoDialogOpen(true);
+  }, [setIsInfoDialogOpen]);
+
   return (
     <div className={styles.container}>
+      <InfoDialog
+        isOpen={isInfoDialogOpen}
+        onClose={() => {
+          setIsInfoDialogOpen(false);
+        }}
+      />
       <Stack
         direction="column"
         alignItems="center"
@@ -56,13 +76,13 @@ const Dashboard = () => {
         <div className={styles.item}>
           <div className={styles.level}>{level}</div>
         </div>
-        <div className={styles.item} onClick={shareText}>
+        <div className={styles.item} onClick={handleShareClicked}>
           <ShareIcon
             fontSize="medium"
             className={classNames(styles.icon, styles.share)}
           />
         </div>
-        <div className={styles.item}>
+        <div className={styles.item} onClick={handleInfoClicked}>
           <InfoIcon
             fontSize="medium"
             className={classNames(styles.icon, styles.info)}
---
+++ src/components/InfoDialog/InfoDialog.tsx
@@ -0,0 +1,117 @@
+import styles from "./InfoDialog.module.scss";
+import Dialog from "@mui/material/Dialog";
+import { Stack } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
+import InstructionsPic from "@/assets/images/instructions_simon_game_pic.png";
+import ReactLogo from "@/assets/images/react.png";
+import ReduxLogo from "@/assets/images/redux.png";
+import SassLogo from "@/assets/images/sass.png";
+import MuiLogo from "@/assets/images/mui.png";
+
+const InfoDialog = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void | undefined;
+}) => {
+  return (
+    <Dialog open={isOpen} onClose={() => onClose()} className={styles.root}>
+      <div className={styles.contentWrapper}>
+        <Stack
+          direction="column"
+          gap={1}
+          justifyContent="space-around"
+          className={styles.contentStack}
+        >
+          <Stack
+            direction="row"
+            className={styles.titleWrapper}
+            justifyContent="space-between"
+          >
+            <div className={styles.title}>
+              REPEAT THE SEQUENCE OF
+              <br />
+              FLASHING LIGHTS & SOUNDS!
+            </div>
+            <button className={styles.closeButton} onClick={() => onClose()}>
+              <CloseIcon className={styles.closeIcon} />
+            </button>
+          </Stack>
+          <div>
+            <p>
+              <a href="https://github.com/furettob/simon">Side project</a>{" "}
+              implemented with: React + Redux, thunks, SCSS, Mui.
+            </p>
+            <p>
+              Some fun challenges came from reproducing small details, e.g. using
+              an SVG mask for the game name at the bottom, or adapt UX from a
+              physical object to a single page digital game.
+            </p>
+          </div>
+          <hr className={styles.redDivider} />
+          <div>
+            <Stack direction="row" gap={2} alignItems="center">
+              <div className={styles.imageWrapper}>
+                <img
+                  alt="Simon Instructions Pic"
+                  src={InstructionsPic}
+                  className={styles.image}
+                />
+              </div>
+              <div className={styles.text}>
+                <p>
+                  Think quickly. React fast. Watch the colored lights flash!
+                  Listen to the sound signals. Repeat them exactly!
+                  Ever-changing and ever-challenging sequences make this classic
+                  electronic memory game unforgettable fun!
+                </p>
+                <ul>
+                  <li>3 EXCITING GAMES</li>
+                  <li>4 DIFFERENT SKILL LEVELS</li>
+                  <li>COMPLETELY PORTABLE </li>
+                </ul>
+              </div>
+            </Stack>
+            <p>
+              Simon Game (<a
+                href="https://en.wikipedia.org/wiki/Simon_(game)"
+                aria-label="Simon wiki page"
+              >
+                Wikipedia
+              </a>) had many models and firmwares. The one
+              reproduced here has 3 game modes and 4 skill levels. Use the
+              switch to change them, and an info message will descrbe the new
+              mode.
+            </p>
+          </div>
+          <hr className={styles.redDivider} />
+          <div>
+            <p className={styles.subtitle}>For more elctronic fun...</p>
+            <Stack direction="row" justifyContent="space-between" >
+              <a className={styles.logoContainer} href="https://react.dev/">
+                  <img src={ReactLogo} alt="React" />
+                  <span>React</span>
+              </a>
+               <a className={styles.logoContainer} href="https://redux.js.org/">
+                  <img src={ReduxLogo} alt="Redux"/>
+                  <span>Redux</span>
+              </a>
+              <a className={styles.logoContainer} href="https://sass-lang.com/">
+                  <img src={SassLogo} alt="Sass"/>
+                  <span>Sass</span>
+              </a>
+               <a className={styles.logoContainer} href="https://mui.com/">
+                  <img src={MuiLogo} alt="Mui"/>
+                  <span>Mui</span>
+              </a>
+            </Stack>
+            <p>Hosted on <a href="https://docs.github.com/en/pages">GitHub Pages</a>. Tracked with <a href="https://developers.google.com/analytics">GA4</a>. Made with fun by <a href="https://github.com/furettob/">FurettoB</a>.</p>
+          </div>
+        </Stack>
+      </div>
+    </Dialog>
+  );
+};
+
+export default InfoDialog;
